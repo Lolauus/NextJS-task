f2c87139ebe85016bf74c7f75c49587c2c606f75
Lolauus <Viktor.persson91@gmail.com>
2024-06-03 10:41:11 +0200

    adding testing

---
+++ pages/addition.test.js
@@ -0,0 +1,16 @@
+// import { render, screen } from "@testing-library/react";
+// import RenderJoke from "@/api/RenderJoke";
+
+// test("Renderjoke, render a div", () => {
+//   render(<RenderJoke />);
+
+//   screen.getByRole("div", {
+//     name: "Category:",
+//   });
+// });
+
+describe("Adding two numbers together", () => {
+  it("should produce their sum", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
